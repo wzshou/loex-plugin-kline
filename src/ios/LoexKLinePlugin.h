e96fb5b8fc3fd47c1983760cca13ce9c7974b83a
Loex2019 <>
2019-03-18 17:38:59 +0800

    第一次

---
+++ src/ios/LoexKLinePlugin.h
@@ -0,0 +1,16 @@
+//
+//  TestPlugin.h
+//  loexKLine
+//
+//  Created by Loex2019 on 2019/3/16.
+//34567890-
+
+#import <Cordova/CDVPlugin.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoexKLinePlugin : CDVPlugin
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ www/LoexKLinePlugin.js
@@ -0,0 +1,21 @@
+cordova.define("wxx.loex.kline", function(require, exports, module) {
+
+	var exec = require('cordova/exec');
+
+	var LoexKLinePlugin = function () {}
+
+	LoexKLinePlugin.prototype.showKLine = function (success,fail,option) {
+     exec(success, fail, 'LoexKLinePlugin', 'showKLine', option);
+
+     };
+	module.exports = new LoexKLinePlugin();
+}
+
+// function TestModel() {};
+
+// TestModel.prototype.showKLine = function (success,fail,option) {
+//      exec(success, fail, 'LoexKLinePlugin', 'showKLine', option);
+// };
+
+// var testModel = new TestModel();
+// module.exports = testModel;
